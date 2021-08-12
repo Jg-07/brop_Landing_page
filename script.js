d1e60e32600e9df7234993ca96b3f2b06380d3ff
jaias <jaiastrobaskar@gmail.com>
2021-08-12 19:32:54 +0530

    🤘RockingOn🚀
    Signed-off-by: jaias <jaiastrobaskar@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let key = document.querySelector(".key")
-let tool1 = document.querySelector(".toolbox")
+let key = document.querySelector(".key");
+let tool1 = document.querySelector(".toolbox");
+let tool2 = document.querySelector(".topbox");
+let body = document.querySelector(".body");
+let key2 = document.querySelector(".key2");
 let i = 0;
 
 key.addEventListener("click", function () {
@@ -10,6 +13,20 @@ key.addEventListener("click", function () {
     else {
         tool1.classList.remove("exp")
         key.classList.remove("rot")
+    }
+    i++;
+})
+
+key2.addEventListener("click", function () {
+    if (i % 2 == 0) {
+        tool2.classList.add("exp2")
+        body.classList.add("endOverflow")
+        key2.classList.add("rot2")
+    }
+    else {
+        tool2.classList.remove("exp2")
+        body.classList.remove("endOverflow")
+        key2.classList.remove("rot2")
     }
     i++;
 })
